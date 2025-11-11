bf561097a7fad7e1f23963feee462a4dc89a9746
GARAM-SHIN <rkfka4291@naver.com>
2025-11-11 14:53:04 +0900

    Chapter4

---
+++ Chapter4/for_list.py
@@ -0,0 +1,5 @@
+# for 반복문과 리스트
+array = [273, 32, 103, 57, 52]
+
+for element in array:    # 리스트에 반복문을 적용
+    print(element)
---
+++ Chapter4/list03.py
@@ -0,0 +1,8 @@
+# 리스트 요소 하나 제거하기
+list_a = [0, 1, 2, 3, 4, 5]
+
+del list_a[1]    # 제거 방법 [1] - del 키워드
+print("dle list_a[1]:", list_a)
+
+list_a.pop(2)   # 제거 방법 [2] - pop()
+print("pop(2):", list_a)
